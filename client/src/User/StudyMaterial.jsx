3525d17b721a873d49bf9a619ce672854658ca31
Yash Pandey <141578029+yash264@users.noreply.github.com>
2024-10-26 15:53:00 +0530

    Update StudyMaterial.jsx

--- client/src/User/StudyMaterial.jsx
+++ client/src/User/StudyMaterial.jsx
@@ -85,7 +85,7 @@ function StudyMaterial(){
                                 return <tr>
                                     <td>*</td>
                                     <td>{data.title}</td>
-                                    <td>{moment(data.date).add(330,"minute").format('Do MMM YYYY, h:mm:ss a')}</td>
+                                    <td>{moment(data.date).format('Do MMM YYYY, h:mm:ss a')}</td>
                                     <td><button className="btn btn-outline-success" onClick={() => showPdf(data.pdf)}>Click here</button></td>
                                 </tr>
                             })
